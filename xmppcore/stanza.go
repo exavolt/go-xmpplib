f484ed5088c9167b4e9d9e90f1c12126e7d4cbde
Fahrezal Effendi <exavolt@gmail.com>
2018-04-05 12:48:52 +0700

    Define all defined stanza error conditions

--- xmppcore/stanza.go
+++ xmppcore/stanza.go
@@ -6,7 +6,7 @@ import (
 
 const StanzasNS = "urn:ietf:params:xml:ns:xmpp-stanzas"
 
-// RFC 6120  8.3.2
+// RFC 6120 § 8.3.2
 const (
 	StanzaErrorTypeAuth     = "auth"
 	StanzaErrorTypeCancel   = "cancel"
@@ -15,7 +15,7 @@ const (
 	StanzaErrorTypeWait     = "wait"
 )
 
-// RFC 6120  8.3.2
+// RFC 6120 § 8.3.2
 //NOTE: the spec says that this element might contain
 // application-specific error condition.
 type StanzaError struct {
@@ -30,12 +30,30 @@ type StanzaErrorCondition struct {
 	XMLName xml.Name // Deliberately un-tagged
 }
 
-// RFC 6120 section 8.3.3
+// RFC 6120 § 8.3.3
 var (
 	StanzaErrorConditionBadRequest            = stanzaErrorCondition("bad-request")
 	StanzaErrorConditionConflict              = stanzaErrorCondition("conflict")
 	StanzaErrorConditionFeatureNotImplemented = stanzaErrorCondition("feature-not-implemented")
+	StanzaErrorConditionForbidden             = stanzaErrorCondition("forbidden")
+	StanzaErrorConditionGone                  = stanzaErrorCondition("gone")
+	StanzaErrorConditionInternalServerError   = stanzaErrorCondition("internal-server-error")
+	StanzaErrorConditionItemNotFound          = stanzaErrorCondition("item-not-found")
+	StanzaErrorConditionJIDMalformed          = stanzaErrorCondition("jid-malformed")
+	StanzaErrorConditionNotAcceptable         = stanzaErrorCondition("not-acceptable")
+	StanzaErrorConditionNotAllowed            = stanzaErrorCondition("not-allowed")
+	StanzaErrorConditionNotAuthorized         = stanzaErrorCondition("not-authorized")
+	StanzaErrorConditionPolicyViolation       = stanzaErrorCondition("policy-violation")
+	StanzaErrorConditionRecipientUnavailable  = stanzaErrorCondition("recipient-unavailable")
+	StanzaErrorConditionRedirect              = stanzaErrorCondition("redirect")
+	StanzaErrorConditionRegistrationRequired  = stanzaErrorCondition("registration-required")
+	StanzaErrorConditionRemoteServerNotFound  = stanzaErrorCondition("remote-server-not-found")
+	StanzaErrorConditionRemoteServerTimeout   = stanzaErrorCondition("remote-server-timeout")
+	StanzaErrorConditionResourceConstraint    = stanzaErrorCondition("resource-constraint")
 	StanzaErrorConditionServiceUnavailable    = stanzaErrorCondition("service-unavailable")
+	StanzaErrorConditionSubscriptionRequired  = stanzaErrorCondition("subscription-required")
+	StanzaErrorConditionUndefinedCondition    = stanzaErrorCondition("undefined-condition")
+	StanzaErrorConditionUnexpectedRequest     = stanzaErrorCondition("unexpected-request")
 )
 
 func stanzaErrorCondition(local string) StanzaErrorCondition {
