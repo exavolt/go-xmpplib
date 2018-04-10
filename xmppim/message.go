eab3f899f9e91666d27690c01d126ee6addcdc7e
Fahrezal Effendi <exavolt@gmail.com>
2018-04-10 15:10:47 +0700

    Refactor ClientMessage

--- xmppim/message.go
+++ xmppim/message.go
@@ -22,24 +22,40 @@ const (
 	MessageTypeNormal    = "normal"
 )
 
-type ClientMessage struct {
+// ClientMessageAttributes is the base structure for a message stanza. It defines
+// all standard attributes.
+type ClientMessageAttributes struct {
 	XMLName xml.Name      `xml:"jabber:client message"`
 	ID      string        `xml:"id,attr,omitempty"`
 	From    *xmppcore.JID `xml:"from,attr,omitempty"`
 	To      *xmppcore.JID `xml:"to,attr,omitempty"`
 	Type    string        `xml:"type,attr"` // Any of MessageType*
-	Payload []byte        `xml:",innerxml"` //TODO: this is inefficient. we should stream this
+}
+
+// ClientMessage is a data structure for a message stanza. It contains
+// all standard attributes and sub-elements.
+type ClientMessage struct {
+	ClientMessageAttributes
+	Error   *xmppcore.StanzaError `xml:",omitempty"`
+	Body    []ClientMessageBody    `xml:"body"`
+	Subject []ClientMessageSubject `xml:"subject"`
+	Thread  *ClientMessageThread   `xml:"thread"`
 }
 
 type ClientMessageBody struct {
 	XMLName xml.Name `xml:"jabber:client body"`
+	Lang    string   `xml:"lang,attr"`
+	Text    string   `xml:",chardata"`
 }
 
 type ClientMessageSubject struct {
 	XMLName xml.Name `xml:"jabber:client subject"`
+	Lang    string   `xml:"lang,attr"`
+	Text    string   `xml:",chardata"`
 }
 
 type ClientMessageThread struct {
 	XMLName xml.Name `xml:"jabber:client thread"`
 	Parent  string   `xml:"parent,attr"`
+	Text    string   `xml:",chardata"`
 }
