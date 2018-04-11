92f06d720e083794f2c9fc685500a49946f4b69f
Fahrezal Effendi <exavolt@gmail.com>
2018-04-11 22:14:35 +0700

    Add basic tests for xmppsid

---
+++ xmppsid/xmppsid_test.go
@@ -0,0 +1,25 @@
+package xmppsid
+
+import (
+	"encoding/xml"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/exavolt/go-xmpplib/xmppcore"
+)
+
+func TestMarshalEmptyStanzaID(t *testing.T) {
+	var stanzaID StanzaID
+	assert.Panics(t, func() { xml.Marshal(&stanzaID) })
+}
+
+func TestMarshalBasicStanzaID(t *testing.T) {
+	stanzaID := StanzaID{By: &xmppcore.JID{}}
+	xmlBuf, err := xml.Marshal(&stanzaID)
+
+	assert.Nil(t, err)
+	assert.Equal(t,
+		`<stanza-id xmlns="urn:xmpp:sid:0" id="" by=""></stanza-id>`,
+		string(xmlBuf))
+}
